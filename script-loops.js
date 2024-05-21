761c7edeb49ee068039a3e0343ada7b6c4d6c0f7
CR Taylor <157721366+crtaylordev@users.noreply.github.com>
2024-05-20 21:11:00 -0400

    Run basic loops on click; apply less awful colors

--- script-loops.js
+++ script-loops.js
@@ -1,30 +1,17 @@
-const coll = document.getElementsByClassName("faq-collapse");
-let i;
-
-for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        let content = this.nextElementSibling;
-        if (content.style.display === "block") {
-            content.style.display = "none";
-        } else {
-            content.style.display = "block";
-        }
-    });
-}
-
+const basicButton = document.getElementById("basic-button");
 let j;
 
-for(j = 0; j < 3; j++) {
-    alert(j);
-}
+basicButton.addEventListener("click", () => {
+    for (j = 0; j < 3; j++) {
+        alert(j);
+    }
+});
 
-let guys = [stede, ed, buttons, frenchie, izzy, roach, jim];
-let guysButtons = document.querySelectorAll("guys-button");
+const guys = ["Stede", "Ed", "Buttons", "Frenchie", "Izzy", "Roach", "Jim"];
+const genButton = document.getElementById("gen-button");
 
-forEach(guysButton => {
-    guysButton.addEventListener("click", function () {
-        alert(guys);
-    });
-}
-);
+genButton.addEventListener("click", () => {
+    for (const guy of guys) {
+        alert(guy);
+    }
+});
